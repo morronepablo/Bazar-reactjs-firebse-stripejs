27d1c3082f4dea7b0124ec79798fc030b7b7d7cc
morronepablo <morronepablo@gmail.com>
2023-12-01 18:35:06 -0300

    guardando componente Product

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from "react-router-dom";
 import Cart from "./pages/Cart";
 import { productsData } from "./api/Api";
 import Product from "./components/Product";
@@ -11,6 +16,7 @@ const Layout = () => {
   return (
     <div>
       <Header />
+      <ScrollRestoration />
       <Outlet />
       <Footer />
     </div>
